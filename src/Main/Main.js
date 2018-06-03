85240db49f1995a9ca83a0eaaeb2d33c8ec6c09f
Justin Robinson <jurobn@amazon.com>
2018-06-02 21:08:12 -0700

    [master] add router and split app into Header and Main

---
+++ src/Main/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <span>
+        this is where the app content will go
+      </span>
+    );
+  }
+}
+
+export default Main;
