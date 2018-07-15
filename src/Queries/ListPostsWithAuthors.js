6a1d18a6fca2ea8ec15b65d18ec833249d2d16bd
Justin Robinson <jurobn@amazon.com>
2018-07-15 18:32:00 -0400

    [master] update schema and format graphql queries

--- src/Queries/ListPostsWithAuthors.js
+++ src/Queries/ListPostsWithAuthors.js
@@ -1,20 +1,20 @@
 import gql from 'graphql-tag';
 
 export default gql`
-    {
-        listPost(first: 5) {
-            items {
-                postId
-                title
-                content
-                authorId
-                author {
-                    authorId
-                    firstName
-                    lastName
-                    username
-                }
-            }
+  {
+    listPost(first: 5) {
+      items {
+        postId
+        title
+        content
+        authorId
+        author {
+          authorId
+          firstName
+          lastName
+          username
         }
+      }
     }
+  }
 `
